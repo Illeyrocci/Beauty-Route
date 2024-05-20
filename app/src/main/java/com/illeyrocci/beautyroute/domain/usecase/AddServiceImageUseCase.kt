4ccfd8fd9aa0341f3ab566e6f4bc04cbdbca3e6e
Illeyrocci <40275990+Illeyrocci@users.noreply.github.com>
2024-05-20 19:40:00 +0300

    displaying services' gallery

---
+++ app/src/main/java/com/illeyrocci/beautyroute/domain/usecase/AddServiceImageUseCase.kt
@@ -0,0 +1,23 @@
+package com.illeyrocci.beautyroute.domain.usecase
+
+import android.net.Uri
+import com.illeyrocci.beautyroute.domain.repository.AuthRepository
+import com.illeyrocci.beautyroute.domain.repository.MediaRepository
+import com.illeyrocci.beautyroute.domain.repository.UserRepository
+
+class AddServiceImageUseCase(
+    private val authRepository: AuthRepository,
+    private val userRepository: UserRepository,
+    private val mediaRepository: MediaRepository
+) {
+    suspend operator fun invoke(position: Int, uri: Uri) {
+        val uid = authRepository.getMyUID().data!!
+        val services = userRepository.getUserDataSnapshot(uid).services
+        val path =
+            "/service_photo/${uid}/${services[position].uid}}"
+
+        mediaRepository.putImage(uri, path)
+
+        userRepository.updateServicePhotos(position, mediaRepository.getAllUrls(path), uid)
+    }
+}
